dc4b127acf32e0ea4a678b5166ae5972bc1c085a
dovotori <web-asst@iMac-de-web-asst-2.local>
2014-02-24 17:05:13 +0100

    Mise en ligne

---
+++ javascript/utils.js
@@ -0,0 +1,37 @@
+
+function getRandom(min, max) 
+{
+    return Math.random() * (max - min) + min;
+}
+
+
+function map(valeur, minRef, maxRef, minDest, maxDest) {
+  return minDest + (valeur - minRef) * (maxDest - minDest) / (maxRef - minRef);
+}
+
+
+function signe(valeur)
+{
+  if(valeur == 0)
+    return 0;
+  else if(valeur > 0)
+    return 1;
+  else
+    return -1;
+}
+
+
+
+
+function lerp(t, a, b){
+    return (1-t)*a + t*b;
+}
+
+
+
+
+
+
+
+
+
