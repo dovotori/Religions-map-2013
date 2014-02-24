031a84dbf29811ebfb5106861f1ff403f4f8d4b7
dovotori <web-asst@iMac-de-web-asst-2.local>
2014-02-24 17:06:58 +0100

    Revert 3df42b6..dc4b127
    This rolls back to commit 3df42b6aa15dfec59253b21f120d718e78d09794.

--- index.php
+++ index.php
@@ -1,53 +1,24 @@
-<!doctype html>
-<html lang="fr">
-	<head>
-		<title>Reporters sans frontières</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+if (!isset($langue_user) && !isset($_SESSION["lang"])){
+	
+	$langue_user=substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+	switch($langue_user){	
 
-		<script type="text/javascript" src="http://rsf.org/squelettes/lib/js/jquery-1.4.2.min.js"></script>
-		<script type="text/javascript" src="javascript/d3.js"></script>
-		<script type="text/javascript" src="javascript/d3geo.js"></script>
-		<script type="text/javascript" src="javascript/queue.js"></script>
-		<script type="text/javascript" src="javascript/main.js"></script>
-		<script type="text/javascript" src="javascript/utils.js"></script>
-
-		<link rel="stylesheet" type="text/css" href="style.css" />
-
-		<style type="text/css">
-
-			
-
-		</style>
-
-
-	</head>
-	<body>
-
-
-		<section id="cp" class="line">
-
-			<h2>Rapport</h2>
-			<p id="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eleifend lorem in turpis feugiat varius. Mauris sit amet molestie erat. Ut accumsan ipsum ut sollicitudin molestie. Nulla aliquet, ligula et pellentesque sodales, enim nibh lobortis massa, sed luctus ligula odio in massa. Donec vel posuere mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras dignissim sem in mi varius, ac tincidunt nibh bibendum. Pellentesque ipsum felis, suscipit sed rutrum sit amet, varius at urna. Integer risus magna, lacinia quis auctor id, vehicula at enim. Nam laoreet quam in sapien egestas, id feugiat elit ultricies. Donec nisi lacus, tincidunt non purus quis, lacinia dictum eros. Vivamus porta ornare sodales. Sed rhoncus commodo odio et vulputate. Cras adipiscing vestibulum dictum. Aliquam non scelerisque magna, vitae semper arcu. Etiam facilisis ultricies purus at luctus. </p>
-			<h2>Ennemis d'Internet</h2>
-		</section>
+	case"fr":
+	$_SESSION["lang"] = "fr";
+	header ("Location: /religions/fr.html");	
+	break;
 		
+	case "en":
+	$_SESSION["lang"] = "en";
+	header ("Location: /religions/en.html");
+	break;
+  	
+	default:
+	$_SESSION["lang"] = "en";
+	header ("Location: /religions/en.html");
+	break;
+	}
+}
 
-		<section id="carte" class="line">		
-			<svg id="svgCarte"></svg>
-
-			
-			
-		<?php include_once("articles_fr.php"); ?>
-
-
-		</section>
-
-
-		<section id="sources" class="line">
-		</section>
-
-
-	</body>
-</html>
-
+?>
